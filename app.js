211020f12b6972e8aabce28f84e7608118d4344a
Santhosh Muthoju <santhosh.muthoju@bredec.com>
2024-09-17 19:52:28 +0530

    api's unpdated

--- app.js
+++ app.js
@@ -63,14 +63,12 @@ test.put('/api/courses/:id', (req, res) => {
 })
 
 test.delete('/api/courses/:id', (req, res) => {
-    const course  =  courses.find(c => c.id === parseInt(req.params.id))
-    if(!course) { 
-        return res.status(404).send('The course with the given id is not found!')
-    }
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('Invalid id provided !');
+
     const index = courses.indexOf(course);
     courses.splice(index, 1);
-
-    res.send(course) /////
+    res.send(course);
 })
 
 
